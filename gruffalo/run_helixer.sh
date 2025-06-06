5f273440489572597553059098c064e204f82ef3
Thomas Heaven <theaven@gruffalo.hpc.hutton.ac.uk>
2025-06-06 08:57:40 +0100

    Changes committed: Fri  6 Jun 08:57:04 BST 2025

--- gruffalo/run_helixer.sh
+++ gruffalo/run_helixer.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=helixer
+#SBATCH -o slurm.%j.out
+#SBATCH -e slurm.%j.err
 #SBATCH --partition=gpu
 #SBATCH --gpus=1
-#SBATCH --c 32
+#SBATCH --cpus-per-task=32
 #SBATCH --mem-per-cpu=6G
 #SBATCH --time=03-00:00:00
 
@@ -38,40 +40,40 @@ conda activate tensorflow-gpu-env
 if [ "$lineage" = "fungi" ]; then
   if [ -e "$model_filepath" ]; then
     echo "running with pre-downloaded model: $model_filepath"
-    singularity run --nv /home/theaven/scratch/apps/helixer/helixer-docker_helixer_v0.3.2_cuda_11.8.0-cudnn8.sif Helixer.py --model-filepath $model_filepath --fasta-path $fasta --subsequence-length 21384 --overlap-offset 10692 --overlap-core-length 16038 --species $species --gff-output-path ${outfile}.gff
+    singularity exec --nv /home/theaven/scratch/apps/helixer/helixer-docker_helixer_v0.3.2_cuda_11.8.0-cudnn8.sif Helixer.py --model-filepath $model_filepath --fasta-path $fasta --subsequence-length 21384 --overlap-offset 10692 --overlap-core-length 16038 --species $species --gff-output-path ${outfile}.gff
   else
     echo "pulling model for: $lineage"
-    singularity run --nv /home/theaven/scratch/apps/helixer/helixer-docker_helixer_v0.3.2_cuda_11.8.0-cudnn8.sif Helixer.py --fasta-path $fasta --lineage fungi --subsequence-length 21384 --overlap-offset 10692 --overlap-core-length 16038 --species $species --gff-output-path ${outfile}.gff
+    singularity exec --nv /home/theaven/scratch/apps/helixer/helixer-docker_helixer_v0.3.2_cuda_11.8.0-cudnn8.sif Helixer.py --fasta-path $fasta --lineage fungi --subsequence-length 21384 --overlap-offset 10692 --overlap-core-length 16038 --species $species --gff-output-path ${outfile}.gff
   fi
 
 
 elif [ "$lineage" = "invertebrate" ]; then
   if [ -e "$model_filepath" ]; then
     echo "running with pre-downloaded model: $model_filepath"
-    singularity run --nv /home/theaven/scratch/apps/helixer/helixer-docker_helixer_v0.3.2_cuda_11.8.0-cudnn8.sif Helixer.py --model-filepath $model_filepath --fasta-path $fasta --subsequence-length 213840 --overlap-offset 106920 --overlap-core-length 160380 --species $species --gff-output-path ${outfile}.gff
+    singularity exec --nv /home/theaven/scratch/apps/helixer/helixer-docker_helixer_v0.3.2_cuda_11.8.0-cudnn8.sif Helixer.py --model-filepath $model_filepath --fasta-path $fasta --subsequence-length 213840 --overlap-offset 106920 --overlap-core-length 160380 --species $species --gff-output-path ${outfile}.gff
   else
     echo "pulling model for: $lineage"
-    singularity run --nv /home/theaven/scratch/apps/helixer/helixer-docker_helixer_v0.3.2_cuda_11.8.0-cudnn8.sif Helixer.py --fasta-path $fasta --lineage invertebrate --subsequence-length 213840 --overlap-offset 106920 --overlap-core-length 160380 --species $species --gff-output-path ${outfile}.gff
+    singularity exec --nv /home/theaven/scratch/apps/helixer/helixer-docker_helixer_v0.3.2_cuda_11.8.0-cudnn8.sif Helixer.py --fasta-path $fasta --lineage invertebrate --subsequence-length 213840 --overlap-offset 106920 --overlap-core-length 160380 --species $species --gff-output-path ${outfile}.gff
   fi
 
 
 elif [ "$lineage" = "vertebrate" ]; then
   if [ -e "$model_filepath" ]; then
     echo "running with pre-downloaded model: $model_filepath"
-    singularity run --nv /home/theaven/scratch/apps/helixer/helixer-docker_helixer_v0.3.2_cuda_11.8.0-cudnn8.sif Helixer.py --model-filepath $model_filepath --fasta-path $fasta --subsequence-length 213840 --overlap-offset 106920 --overlap-core-length 160380 --species $species --gff-output-path ${outfile}.gff
+    singularity exec --nv /home/theaven/scratch/apps/helixer/helixer-docker_helixer_v0.3.2_cuda_11.8.0-cudnn8.sif Helixer.py --model-filepath $model_filepath --fasta-path $fasta --subsequence-length 213840 --overlap-offset 106920 --overlap-core-length 160380 --species $species --gff-output-path ${outfile}.gff
   else
     echo "pulling model for: $lineage"
-    singularity run --nv /home/theaven/scratch/apps/helixer/helixer-docker_helixer_v0.3.2_cuda_11.8.0-cudnn8.sif Helixer.py --fasta-path $fasta --lineage vertebrate --subsequence-length 213840 --overlap-offset 106920 --overlap-core-length 160380 --species $species --gff-output-path ${outfile}.gff
+    singularity exec --nv /home/theaven/scratch/apps/helixer/helixer-docker_helixer_v0.3.2_cuda_11.8.0-cudnn8.sif Helixer.py --fasta-path $fasta --lineage vertebrate --subsequence-length 213840 --overlap-offset 106920 --overlap-core-length 160380 --species $species --gff-output-path ${outfile}.gff
   fi
 
 
 elif [ "$lineage" = "land_plant" ]; then
   if [ -e "$model_filepath" ]; then
     echo "running with pre-downloaded model: $model_filepath"
-    singularity run --nv /home/theaven/scratch/apps/helixer/helixer-docker_helixer_v0.3.2_cuda_11.8.0-cudnn8.sif Helixer.py --model-filepath $model_filepath --fasta-path $fasta --subsequence-length 21384 --overlap-offset 10692 --overlap-core-length 16038 --species $species --gff-output-path ${outfile}.gff
+    singularity exec --nv /home/theaven/scratch/apps/helixer/helixer-docker_helixer_v0.3.2_cuda_11.8.0-cudnn8.sif Helixer.py --model-filepath $model_filepath --fasta-path $fasta --subsequence-length 21384 --overlap-offset 10692 --overlap-core-length 16038 --species $species --gff-output-path ${outfile}.gff
   else
     echo "pulling model for: $lineage"
-    singularity run --nv /home/theaven/scratch/apps/helixer/helixer-docker_helixer_v0.3.2_cuda_11.8.0-cudnn8.sif Helixer.py --fasta-path $fasta --lineage land_plant --subsequence-length 21384 --overlap-offset 10692 --overlap-core-length 16038 --species $species --gff-output-path ${outfile}.gff
+    singularity exec --nv /home/theaven/scratch/apps/helixer/helixer-docker_helixer_v0.3.2_cuda_11.8.0-cudnn8.sif Helixer.py --fasta-path $fasta --lineage land_plant --subsequence-length 21384 --overlap-offset 10692 --overlap-core-length 16038 --species $species --gff-output-path ${outfile}.gff
   fi
 
 else
